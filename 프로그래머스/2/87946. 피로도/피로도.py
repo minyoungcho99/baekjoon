7248a4a0b52688ab051135e06cdf58b63126f384
minyoungcho99 <116537478+minyoungcho99@users.noreply.github.com>
2024-11-30 16:43:15 -0500

    [level 2] Title: 피로도, Time: 61.60 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,22 +1,20 @@
-# 피로도
-from itertools import permutations
+
+result = []
+max_n = 0
+
+
+def backtrack(k, dungeons):
+    global max_n
+
+    max_n = max(max_n, len(result))
+
+    for i in range(len(dungeons)):
+        if i not in result and dungeons[i][0] <= k: 
+            result.append(i)
+            backtrack(k - dungeons[i][1], dungeons) 
+            result.pop()
 
 
 def solution(k, dungeons):
-    max_num = 0
-
-    for order in permutations(range(len(dungeons))):
-        temp = 0
-        temp_k = k
-        for i in order:
-            if dungeons[i][0] <= temp_k:
-                temp_k -= dungeons[i][1]
-                temp += 1
-                continue
-
-            else:
-                break
-
-        max_num = max(max_num, temp)
-
-    return max_num
+    backtrack(k, dungeons)
+    return max_n
