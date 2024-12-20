6919cbabbda774cf75c28c8599d1654c80bf0877
minyoungcho99 <116537478+minyoungcho99@users.noreply.github.com>
2024-12-20 17:04:18 -0500

    Update 체육복.py

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,20 +1,20 @@
 def solution(n, lost, reserve):
-    answer = 0
-    
-    for l in lost:
-        print(l)
-        if l in reserve:
+    for l in lost[:]:
+        if l in reserve[:]:
             lost.remove(l)
             reserve.remove(l)
-        
-        if l-1 in reserve:
-            lost.remove(l)
-            reserve.remove(l-1)
-        elif l+1 in reserve:
-            lost.remove(l)
-            reserve.remove(l+1)
-    
-    print(lost)
-    answer = n - len(lost)
-    
-    return answer
+
+    temp = len(lost)
+
+    for l in sorted(lost):
+        if l - 1 in reserve:
+            reserve.remove(l - 1)
+            temp -= 1
+            continue
+
+        elif l + 1 in reserve:
+            reserve.remove(l + 1)
+            temp -= 1
+            continue
+
+    return n - temp
