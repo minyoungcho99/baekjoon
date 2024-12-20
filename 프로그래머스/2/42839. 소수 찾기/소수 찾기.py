302f84630fb325e3d00f7a13afbf1d7cd465d3a6
minyoungcho99 <116537478+minyoungcho99@users.noreply.github.com>
2024-12-20 00:23:01 -0500

    [level 2] Title: 소수 찾기, Time: 10.78 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,11 +1,11 @@
 # 소수 찾기
-from itertools import permutations
 from math import sqrt
 
+
 def solution(numbers):
-    nums = list(map(int, numbers))
-    ans = 0
-    num_list = []
+    nums = list(numbers)
+    visited = [False] * len(nums)
+    unique_numbers = set()
 
     def is_prime(num):
         if num <= 1:
@@ -17,19 +17,16 @@ def solution(numbers):
 
         return True
 
-    for i in range(1, len(nums) + 1):
-        for j in permutations(nums, i):
-            try:
-                num = int("".join(map(str, j)))
-                if num not in num_list:
-                    num_list.append(num)
+    def backtrack(current):
+        if current:
+            unique_numbers.add(int(current))
 
-            except:
-                continue
+        for i in range(len(nums)):
+            if not visited[i]:
+                visited[i] = True
+                backtrack(current + nums[i])
+                visited[i] = False
 
+    backtrack("")
 
-    for num in num_list:
-        if is_prime(num):
-            ans += 1
-
-    return ans
+    return sum(1 for num in unique_numbers if is_prime(num))
