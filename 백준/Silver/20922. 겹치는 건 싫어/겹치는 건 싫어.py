05b854c3b66f75bd5a775040bf2bf5cd561cb137
minyoungcho99 <116537478+minyoungcho99@users.noreply.github.com>
2025-01-20 07:42:45 -0500

    [Silver I] Title: 겹치는 건 싫어, Time: 192 ms, Memory: 149492 KB -BaekjoonHub

---
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
@@ -0,0 +1,21 @@
+# 겹치는 건 싫어
+import sys
+from collections import defaultdict
+
+freq = defaultdict(int)
+N, K = map(int, sys.stdin.readline().split())
+nums = list(map(int, sys.stdin.readline().split()))
+
+en = 0
+max_len = 0
+
+for st in range(N):
+    while en < N and freq[nums[en]] < K:
+        freq[nums[en]] += 1
+        en += 1
+
+    max_len = max(max_len, en - st)
+
+    freq[nums[st]] -= 1
+
+print(max_len)
