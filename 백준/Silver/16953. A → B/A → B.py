405cfedfbb225d3fb85f54c04c5de1e5be7f0436
minyoungcho99 <116537478+minyoungcho99@users.noreply.github.com>
2025-02-19 16:50:29 +0900

    [Silver II] Title: A → B, Time: 120 ms, Memory: 112604 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -2,22 +2,20 @@
 import sys
 from collections import deque
 
-A, B = map(int, sys.stdin.readline().split())
 
-
-def bfs(curr, target):
-    q = deque([(curr, 1)])
+def bfs(a, b):
+    q = deque([(a, 1)])
 
     while q:
-        x, t = q.popleft()
-        if x == target:
-            return t
-
-        if x * 2 <= target:
-            q.append((x*2, t+1))
-        if x * 10 + 1 <= target:
-            q.append((x * 10 + 1, t+1))
+        x, cnt = q.popleft()
+        if x == b:
+            return cnt
+        elif x < b:
+            q.append((x * 2, cnt + 1))
+            q.append(((int(str(x) + "1")), cnt + 1))
 
     return -1
 
+
+A, B = map(int, sys.stdin.readline().split())
 print(bfs(A, B))
