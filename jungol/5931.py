d4378f86f37dbdaa788c1c99b1f6b80dcd55c6ae
minyoungcho99 <116537478+minyoungcho99@users.noreply.github.com>
2025-03-03 18:40:19 +0900

    Create 5931.py

---
+++ jungol/5931.py
@@ -0,0 +1,12 @@
+# 숫자 사각형
+import sys
+
+N = int(sys.stdin.readline())
+sqr = [[0] * N for _ in range(N)]
+
+for j in range(N):
+    for i in range(N):
+        sqr[i][j] = i+1
+
+for s in sqr:
+    print(" ".join(map(str, s)))
