e119aaa382517719258e4db13c393387d6a40a9f
minyoungcho99 <116537478+minyoungcho99@users.noreply.github.com>
2025-03-05 01:28:34 +0900

    [Gold IV] Title: 테트로미노, Time: 904 ms, Memory: 117508 KB -BaekjoonHub

---
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -0,0 +1,155 @@
+# 테트로미노
+import sys
+from copy import deepcopy
+
+
+def rotate(t):
+    n, m = len(t), len(t[0])
+    temp_90 = [[0] * n for _ in range(m)]
+    temp_180 = [[0] * m for _ in range(n)]
+    temp_270 = [[0] * n for _ in range(m)]
+
+    for i in range(n):
+        for j in range(m):
+            temp_90[j][n - i - 1] = t[i][j]
+
+    for i in range(n):
+        for j in range(m):
+            temp_180[n - i - 1][m - j - 1] = t[i][j]
+
+    for i in range(n):
+        for j in range(m):
+            temp_270[m - j - 1][i] = t[i][j]
+
+    return temp_90, temp_180, temp_270
+
+
+def symme(t):
+    sym = []
+
+    for i in range(len(t)):
+        sym.append(t[i][::-1])
+
+    return sym
+
+
+def find_max(t, t1, t2, t3):
+    max_score = float('-inf')
+
+    a, b = len(t), len(t[0])
+    for sx in range(N - a + 1):
+        for sy in range(M - b + 1):
+            temp = 0
+            for i in range(a):
+                for j in range(b):
+                    if t[i][j] == 1:
+                        temp += board[sx + i][sy + j]
+
+            max_score = max(max_score, temp)
+
+    sym = symme(t)
+    a, b = len(t), len(t[0])
+    for sx in range(N - a + 1):
+        for sy in range(M - b + 1):
+            temp = 0
+            for i in range(a):
+                for j in range(b):
+                    if sym[i][j] == 1:
+                        temp += board[sx + i][sy + j]
+
+            max_score = max(max_score, temp)
+
+    a, b = len(t1), len(t1[0])
+    for sx in range(N - a + 1):
+        for sy in range(M - b + 1):
+            temp = 0
+            for i in range(a):
+                for j in range(b):
+                    if t1[i][j] == 1:
+                        temp += board[sx + i][sy + j]
+
+            max_score = max(max_score, temp)
+
+    sym_90 = symme(t1)
+    a, b = len(t1), len(t1[0])
+    for sx in range(N - a + 1):
+        for sy in range(M - b + 1):
+            temp = 0
+            for i in range(a):
+                for j in range(b):
+                    if sym_90[i][j] == 1:
+                        temp += board[sx + i][sy + j]
+
+            max_score = max(max_score, temp)
+
+    a, b = len(t2), len(t2[0])
+    for sx in range(N - a + 1):
+        for sy in range(M - b + 1):
+            temp = 0
+            for i in range(a):
+                for j in range(b):
+                    if t2[i][j] == 1:
+                        temp += board[sx + i][sy + j]
+
+            max_score = max(max_score, temp)
+
+    sym_180 = symme(t2)
+    a, b = len(t2), len(t2[0])
+    for sx in range(N - a + 1):
+        for sy in range(M - b + 1):
+            temp = 0
+            for i in range(a):
+                for j in range(b):
+                    if sym_180[i][j] == 1:
+                        temp += board[sx + i][sy + j]
+
+            max_score = max(max_score, temp)
+
+    a, b = len(t3), len(t3[0])
+    for sx in range(N - a + 1):
+        for sy in range(M - b + 1):
+            temp = 0
+            for i in range(a):
+                for j in range(b):
+                    if t3[i][j] == 1:
+                        temp += board[sx + i][sy + j]
+
+            max_score = max(max_score, temp)
+
+    sym_270 = symme(t3)
+    a, b = len(t3), len(t3[0])
+    for sx in range(N - a + 1):
+        for sy in range(M - b + 1):
+            temp = 0
+            for i in range(a):
+                for j in range(b):
+                    if sym_270[i][j] == 1:
+                        temp += board[sx + i][sy + j]
+
+            max_score = max(max_score, temp)
+
+    return max_score
+
+
+te = [[[1, 1, 1, 1]],
+    [[1, 1], [1, 1]],
+    [[1, 0], [1, 0], [1, 1]],
+    [[1, 0], [1, 1], [0, 1]],
+      [[1, 1, 1], [0, 1, 0]]]
+
+max_score = float('-inf')
+
+N, M = map(int, sys.stdin.readline().split())
+board = []
+
+for _ in range(N):
+    board.append(list(map(int, sys.stdin.readline().split())))
+
+for t in te:
+    temp = deepcopy(t)
+    rotated_90, rotated_180, rotated_270 = rotate(temp)
+    temp_max = find_max(temp, rotated_90, rotated_180, rotated_270)
+
+    max_score = max(max_score, temp_max)
+
+print(max_score)
