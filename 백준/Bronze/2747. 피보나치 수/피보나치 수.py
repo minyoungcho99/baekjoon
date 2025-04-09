acfc46bd326d2ca3c51a5f002408d227e86d79f5
minyoungcho99 <116537478+minyoungcho99@users.noreply.github.com>
2025-04-09 12:52:28 +0900

    [Bronze II] Title: 피보나치 수, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -0,0 +1,20 @@
+# 피보나치 수 1
+import sys
+
+N = int(sys.stdin.readline())
+dp = [0] * (N+1)
+
+if N <= 1:
+    if N == 0:
+        print(0)
+
+    else:
+        print(1)
+
+else:
+    dp[1]=1
+
+    for i in range(2, N+1):
+        dp[i] = dp[i-1] + dp[i-2]
+
+    print(dp[N])
