aa10048a55f7ce402b19e4ebb82311bfab4fb53c
minyoungcho99 <116537478+minyoungcho99@users.noreply.github.com>
2025-04-18 16:36:47 +0900

    Update updown.py

--- prac/cns_ex/updown.py
+++ prac/cns_ex/updown.py
@@ -1,5 +1,24 @@
 from bisect import bisect_left, bisect_right
 
+# def binary_search(arr, x, c):
+#     st, en = 0, len(arr) - 1
+#     last = -1  # 없을 경우를 고려해서 -1
+
+#     while st <= en:
+#         mid = (st + en) // 2
+
+#         if arr[mid] <= x:
+#             last = mid
+#             st = mid + 1
+#         else:
+#             en = mid - 1
+
+#     if c == 0:  # 다운이라면
+#         return arr[:last+1] 
+
+#     else:  # 업이라면
+#         return arr[last+1:]
+        
 def binary_search(arr, x, c):
     if c == 0:
         return arr[:bisect_left(nums, x)]
